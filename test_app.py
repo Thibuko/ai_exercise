fcf9e48bd5854119fb3909b1006ca44573f731ab
Matthieu Flemal <mafl@hms.se>
2023-10-10 16:54:36 +0200

    unit test

---
+++ test_app.py
@@ -0,0 +1,20 @@
+import unittest
+from app import app  # Replace 'app' with your Flask app instance
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_home_page(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Multiple Choice Quiz', response.data)
+
+    def test_result_page(self):
+        response = self.app.get('/result')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Quiz Results', response.data)
+
+if __name__ == '__main__':
+    unittest.main()
